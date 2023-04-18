06b0f385ee016baa51a904123d9f98e1375d269b
李均 <1961177181@qq.com>
2023-04-18 21:30:33 +0800

    排序算法，插入排序

--- code/src/main/java/com/algorithm/sort/Insert.java
+++ code/src/main/java/com/algorithm/sort/Insert.java
@@ -1,20 +1,44 @@
 package com.algorithm.sort;
 
+import java.util.Arrays;
+
 /**
  * 插入排序
- *    过程：
- *     1.保证0-0上有序
- *     2.保证0-1上有序
- *     3.保证0-2上有序
- *     。。。。。
+ * 过程：
+ * 1.保证0-0上有序
+ * 2.保证0-1上有序
+ * 3.保证0-2上有序
+ * 。。。。。
  */
 public class Insert {
     public static void main(String[] args) {
-
+        int times = 10000;
+        int maxLength = 1000;
+        int maxValue = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < times; i++) {
+            int[] arr = SortUtils.getRandonArr(maxValue, maxLength);
+            int[] arr1 = SortUtils.copyArr(arr);
+            int[] arr2 = SortUtils.copyArr(arr);
+            insertSort(arr1);
+            Arrays.sort(arr2);
+            SortUtils.checkArr(arr,arr1,arr2);
+        }
+        System.out.println("测试结束");
     }
 
-    public static void insertSort(int[] arr){
-
+    public static void insertSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    SortUtils.swap1(arr,j,j-1);
+                }
+            }
+        }
     }
 
 }
